f4ceb2c336c288b8bd2c8ab0d849afe774f0bf8a
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-01 09:22:00 -0300

    Adicionando projetos iniciais

---
+++ desafio02.py
@@ -0,0 +1,5 @@
+dia = input('Qual o dia do nascimento? ')
+mes = input('Qual o mês de nascimento? ')
+ano = input('Qual o ano de nascimento? ')
+
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
---
+++ desafio03.py
@@ -0,0 +1,11 @@
+primeiro = input('Escreva o primeiro número')
+segundo = input('Escreva o segundo número')
+
+print('A soma é:', primeiro + segundo)
+
+print('')
+print('')
+
+soma = primeiro + segundo
+
+print('A soma é:',soma)
