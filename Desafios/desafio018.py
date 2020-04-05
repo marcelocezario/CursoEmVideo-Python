29fd4e192531f9187c971392855600bcc2d6eb3c
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-04 22:08:49 -0300

    Aula 8 - Escolhendo valores aleatórios em lista; Classificando itens aleatoriamente; Adicionando e executando mp3 no projeto

--- Desafios/desafio018.py
+++ Desafios/desafio018.py
@@ -2,8 +2,8 @@ import math
 
 angulo = int(input('Digite um angulo qualquer: '))
 
-seno = math.sin(angulo)
-consseno = math.cos(angulo)
-tangente = math.tan(angulo)
+seno = math.sin(math.radians(angulo))
+consseno = math.cos(math.radians(angulo))
+tangente = math.tan(math.radians(angulo))
 
-print('O seno do angulo de {}º é {}, o cosseno é {} e a tangente é {}'.format(angulo, seno, consseno, tangente))
+print('O seno do angulo de {}º é {:.2f}, o cosseno é {:.2f} e a tangente é {:.2f}'.format(angulo, seno, consseno, tangente))
