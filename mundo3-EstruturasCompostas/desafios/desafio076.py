cbb3200853543af3edabce8ec1c56b10efdabcd7
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-09 21:31:02 -0300

    Aula 16 - Tuplas

---
+++ mundo3-EstruturasCompostas/desafios/desafio076.py
@@ -0,0 +1,5 @@
+'''
+Desafios aula 16
+Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência. No final,
+mostre uma listagem de preços, organizando os dados em forma tabular.
+'''
---
+++ mundo3-EstruturasCompostas/desafios/desafio077.py
@@ -0,0 +1,5 @@
+'''
+Desafios aula 16
+Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada
+palavra, quais são as suas vogais.
+'''
