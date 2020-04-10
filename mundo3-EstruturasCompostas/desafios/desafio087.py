117a9b4e71717c6e0ffb6aa34138d7b7d9e0e1e7
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-09 21:39:58 -0300

    Aula 18 - Listas (parte 2)

---
+++ mundo3-EstruturasCompostas/desafios/desafio087.py
@@ -0,0 +1,7 @@
+'''
+Desafios aula 18
+Aprimore o desafio anterior, mostrando no final:
+A) A soma de todos os valores pares digitados.
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha.
+'''
