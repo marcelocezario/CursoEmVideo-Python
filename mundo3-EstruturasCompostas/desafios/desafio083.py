720c9242464a3dab24e42e45e3fc4926b1dc067e
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-09 21:36:12 -0300

    Aula 17 - Listas (parte 1)

---
+++ mundo3-EstruturasCompostas/desafios/desafio083.py
@@ -0,0 +1,5 @@
+'''
+Desafios aula 17
+Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a
+expressão passada está com os parênteses abertos e fechados na ordem correta.
+'''
